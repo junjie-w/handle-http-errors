d6272a99a7a3987562e76840d981d4bb6d0e98bc
junjie-w <junjiewu.brooke@gmail.com>
2024-12-20 23:27:26 +0100

    fix: update commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,5 +2,8 @@ export default {
   extends: ["@commitlint/config-conventional"],
   rules: {
     "body-max-line-length": [0],
+    "footer-max-line-length": [0],
+    "header-max-length": [0],
+    "subject-case": [0],
   },
 };
